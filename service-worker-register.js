83c3832eb9bf4e3b0239a6ffbe6a9929ecd14503
Thorge Mrowinski <thorge.mrowinski@gmail.com>
2025-11-07 16:24:09 +0100

    Add service worker registration script

---
+++ service-worker-register.js
@@ -0,0 +1,8 @@
+// Service Worker Registration
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(reg => console.log('✓ Service Worker registered'))
+            .catch(err => console.log('✗ Service Worker failed:', err));
+    });
+}
